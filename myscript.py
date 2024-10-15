245302211b74112abf27a9f4b75a6f17eea0b7e5
YASHMARMAT <yashmarmat08@gmail.com>
2024-10-15 14:05:58 +0530

    added db and models

--- myscript.py
+++ myscript.py
@@ -1,10 +1,38 @@
+import os
 from flask import Flask
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
 
 # to read .env files
-# load_dotenv()
+load_dotenv()
 
+# configs
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "my random secret key"
+basedir = os.path.abspath(os.path.dirname(__file__))  # project's root directory
+sqlite_db_path = os.path.join(basedir, "data.sqlite")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + sqlite_db_path
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+# Models 
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
 
 @app.route('/')
 def homepage():
